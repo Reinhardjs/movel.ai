8c44e3c2b918c2c35bab1bbb18460d60c822bbed
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-26 18:46:24 +0700

    added pen drawing functionality

--- frontend/src/core/Canvas.tsx
+++ frontend/src/core/Canvas.tsx
@@ -1,5 +1,5 @@
-import { useRef, useCallback } from "react";
-import { Layer, Stage } from "react-konva";
+import React, { useRef, useCallback } from "react";
+import { Layer, Line, Stage, Text } from "react-konva";
 
 import {
     useShapes,
@@ -16,6 +16,36 @@ import { ShapeProp } from "./shapeProp";
 const handleDragOver = (event: any) => event.preventDefault();
 
 export function Canvas() {
+    const [tool, setTool] = React.useState<any>('pen');
+    const [lines, setLines] = React.useState<any>([]);
+    const isDrawing = React.useRef(false);
+
+    const handleMouseDown = (e: any) => {
+        isDrawing.current = true;
+        const pos = e.target.getStage().getPointerPosition();
+        setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+    };
+
+    const handleMouseMove = (e: any) => {
+        // no drawing - skipping
+        if (!isDrawing.current) {
+            return;
+        }
+        const stage = e.target.getStage();
+        const point = stage.getPointerPosition();
+        let lastLine = lines[lines.length - 1];
+        // add point
+        lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+        // replace last
+        lines.splice(lines.length - 1, 1, lastLine);
+        setLines(lines.concat());
+    };
+
+    const handleMouseUp = () => {
+        isDrawing.current = false;
+    };
+
     const shapes = useShapes((state) => Object.entries(state.shapes));
 
     const stageRef = useRef<any>();
@@ -59,10 +89,28 @@ export function Canvas() {
                 width={window.innerWidth - 400}
                 height={window.innerHeight}
                 onClick={clearSelection}
+                onMouseDown={handleMouseDown}
+                onMousemove={handleMouseMove}
+                onMouseup={handleMouseUp}
             >
                 <Layer>
                     {shapes.map(([key, shape]) => (
                         <Shape key={key} shape={{ ...(shape as ShapeProp), id: key }} />
+                    ))}
+                </Layer>
+                <Layer>
+                    <Text text="Just start drawing" x={5} y={30} />
+                    {lines.map((line: any, i: any) => (
+                        <Line
+                            key={i}
+                            points={line.points}
+                            stroke="#df4b26"
+                            strokeWidth={5}
+                            tension={0.5}
+                            lineCap="round"
+                            lineJoin="round"
+                            globalCompositeOperation={'source-over'}
+                        />
                     ))}
                 </Layer>
             </Stage>
