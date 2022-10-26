bb247c567cae84da8eb4bffbc393e8f82ee860d6
reinhardjs <reinhardjsilalahi@gmail.com>
2022-10-26 18:16:08 +0700

    added circle and rectangle shape

---
+++ frontend/src/shapes/Circle.tsx
@@ -0,0 +1,82 @@
+import { useRef, useEffect, useCallback } from "react";
+import { Circle as KonvaCircle, Transformer } from "react-konva";
+
+import { LIMITS } from "../configs/constants";
+import { selectShape, transformCircleShape, moveShape } from "../data/state";
+
+const boundBoxCallbackForCircle = (oldBox: any, newBox: any) => {
+  if (
+    newBox.width < LIMITS.CIRCLE.MIN ||
+    newBox.height < LIMITS.CIRCLE.MIN ||
+    newBox.width > LIMITS.CIRCLE.MAX ||
+    newBox.height > LIMITS.CIRCLE.MAX
+  ) {
+    return oldBox;
+  }
+  return newBox;
+};
+
+export function Circle({ id, isSelected, type, ...shapeProps }: { id: any, isSelected: any, type: any }) {
+  const shapeRef = useRef<any>();
+  const transformerRef = useRef<any>();
+
+  useEffect(() => {
+    if (isSelected) {
+      transformerRef.current.nodes([shapeRef.current]);
+      transformerRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  const handleSelect = useCallback(
+    (event: any) => {
+      event.cancelBubble = true;
+
+      selectShape(id);
+    },
+    [id]
+  );
+
+  const handleDrag = useCallback(
+    (event: any) => {
+      moveShape(id, event);
+    },
+    [id]
+  );
+
+  const handleTransform = useCallback(
+    (event: any) => {
+      transformCircleShape(shapeRef.current, id, event);
+    },
+    [id]
+  );
+
+  return (
+    <>
+      <KonvaCircle
+        onClick={handleSelect}
+        onTap={handleSelect}
+        onDragStart={handleSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={handleDrag}
+        onTransformEnd={handleTransform}
+      />
+      {isSelected && (
+        <Transformer
+          anchorSize={5}
+          borderDash={[6, 2]}
+          ref={transformerRef}
+          rotateEnabled={false}
+          enabledAnchors={[
+            "top-left",
+            "top-right",
+            "bottom-right",
+            "bottom-left",
+          ]}
+          boundBoxFunc={boundBoxCallbackForCircle}
+        />
+      )}
+    </>
+  );
+}
---
+++ frontend/src/shapes/Rectangle.tsx
@@ -0,0 +1,76 @@
+import { useRef, useEffect, useCallback } from "react";
+import { Rect as KonvaRectangle, Transformer } from "react-konva";
+
+import { LIMITS } from "../configs/constants";
+import { selectShape, transformRectangleShape, moveShape } from "../data/state";
+
+const boundBoxCallbackForRectangle = (oldBox: any, newBox: any) => {
+  // limit resize
+  if (
+    newBox.width < LIMITS.RECT.MIN ||
+    newBox.height < LIMITS.RECT.MIN ||
+    newBox.width > LIMITS.RECT.MAX ||
+    newBox.height > LIMITS.RECT.MAX
+  ) {
+    return oldBox;
+  }
+  return newBox;
+};
+
+export function Rectangle({ id, isSelected, type, ...shapeProps }: { id: any, isSelected: any, type: any }) {
+  const shapeRef = useRef<any>();
+  const transformerRef = useRef<any>();
+
+  useEffect(() => {
+    if (isSelected) {
+      transformerRef.current!.nodes([shapeRef.current]);
+      transformerRef.current!.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  const handleSelect = useCallback(
+    (event: any) => {
+      event.cancelBubble = true;
+
+      selectShape(id);
+    },
+    [id]
+  );
+
+  const handleDrag = useCallback(
+    (event: any) => {
+      moveShape(id, event);
+    },
+    [id]
+  );
+
+  const handleTransform = useCallback(
+    (event: any) => {
+      transformRectangleShape(shapeRef.current, id, event);
+    },
+    [id]
+  );
+
+  return (
+    <>
+      <KonvaRectangle
+        onClick={handleSelect}
+        onTap={handleSelect}
+        onDragStart={handleSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={handleDrag}
+        onTransformEnd={handleTransform}
+      />
+      {isSelected && (
+        <Transformer
+          anchorSize={5}
+          borderDash={[6, 2]}
+          ref={transformerRef}
+          boundBoxFunc={boundBoxCallbackForRectangle}
+        />
+      )}
+    </>
+  );
+}
